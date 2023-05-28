b7493c836899991f716b707f73d6d4a0c0f963e7
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-05-28 12:06:19 +0900

    deploy nft

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -7,8 +7,12 @@ async function main() {
   const ERC6551Registry = await ethers.getContractFactory("ERC6551Registry");
   const erc6551Registry = await ERC6551Registry.deploy();
 
+  const SimpleNFT = await ethers.getContractFactory("SimpleNFT");
+  const simpleNFT = await SimpleNFT.deploy();
+
   await exampleERC6551Account.deployed();
   await erc6551Registry.deployed();
+  await simpleNFT.deployed();
 }
 
 // We recommend this pattern to be able to use async/await everywhere
