7d33f203ca010f1e30d8f9cd6e1df98527e3400a
Tomoya Kobayashi <tomocha.marika@gmail.com>
2023-05-28 11:49:29 +0900

    succeeded to connect localnode

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
+import { ethers } from "ethers";
 
+async function getLatestBlock() {
+  const provider = new ethers.JsonRpcProvider("http://127.0.0.1:8545");
+  const blockNumber = await provider.getBlockNumber();
+  console.log("Current block number: " + blockNumber);
+}
 function App() {
   return (
-    <div className="App">
+    <div className="App" onClick={getLatestBlock}>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
